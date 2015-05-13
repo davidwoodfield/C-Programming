a5d25e0776f6cffc71feea8dc714cc1062677b12
David Woodfield <mr.david.woodfield@gmail.com>
2015-05-13 16:37:43 +0100

    A quick demo of integer overflow

---
+++ gotchas/integer_overflow.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+int main()
+{
+    printf("\n------------------------- START ----------------------------\n");
+    
+    
+    /*
+     * A quick demonstration of integer overflow.
+     *
+     * When the value of (random * pid) is greater than the maximum size of an
+     * integer, integer overflow happens
+     */
+     
+    int pid = 20405;
+    unsigned int seed = (unsigned)time(NULL);
+    int random = 0;
+    int maxint = 0x7FFFFFFF;
+    unsigned int maxuint = 0xFFFFFFFF;
+    
+    printf("\nseed                   =   %u", seed);
+    
+    srand(seed);
+    
+    printf("\nRAND_MAX               =   %d", RAND_MAX);
+    
+    printf("\nMax integer            =   %d", maxint);
+    printf("\nMax unsigned integer   =   %u", maxuint);
+    
+    random = rand();
+    
+    printf("\nrandom number (int)    =   %d", random);
+    printf("\nrandom number (uint)   =   %u", random);
+    
+    printf("\npid                    =   %d", pid);
+    
+    printf("\nrandom x pid (int)     =   %08d", (int)(random * pid));
+    
+    printf("\nrandom x pid (uint)    =   %08u", (unsigned int)(random * pid));
+
+    
+    printf("\n\n------------------------- END ----------------------------\n");
+    
+    return 0;
+}
