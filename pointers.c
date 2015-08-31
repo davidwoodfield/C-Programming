301f1b7d70fe48484a9e19770434f58b5c7081ea
David Woodfield <mr.david.woodfield@gmail.com>
2015-08-31 22:15:49 +0100

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -2,23 +2,50 @@
 int main()
 {
 
-
-int a = 7;
+/*
+ * Intergers
+ */
+int a;
 int *pA = NULL;
 int **ppA = NULL;
 
+/*
+ * Characters and strings
+ */
+char c;
+char s[6] = NULL;
+char *pS = NULL;
+
+/*
+ * integers
+ */
+a = 7;
 pA = &a;
 ppA = &pA;
 
+
+/*
+ * Characters and strings
+ */
+c = 'x';
+strncpy(s,
+        "hello",
+        sizeof(s) - 1);
+pS = &s;
+
+
 printf("\n ------ start of program ------ \n");
 
+printf("\n sizeof of a (sizeof(a)) = %u", sizeof(a));
 printf("\n address of a (&a) = %#08x", &a);
 printf("\n value of a (a) = %d", a);
 
+printf("\n sizeof of a (sizeof(pA)) = %u", sizeof(pA));
 printf("\n address of pA (&pA) = %#08x", &pA);
 printf("\n value of pA (pA) = %#08x", pA);
 printf("\n the value of what pA points to (*pA) = %d", *pA);
 
+printf("\n sizeof of a (sizeof(ppA)) = %u", sizeof(ppA));
 printf("\n address of ppA (&ppA) = %#08x", &ppA);
 printf("\n value of ppA (ppA) = %#08x", ppA);
 printf("\n the value of what ppA points to (*ppA) = %#08x", *ppA);
